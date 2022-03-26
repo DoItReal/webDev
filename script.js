4659a907e31741c626c23c0bc2b80ebaf563d17e
DoItReal <66225172+DoItReal@users.noreply.github.com>
2022-03-26 20:33:19 +0200

    third one

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-function init() {
-
-}
-
 var video, htmlTracks;
 var trackStatusesDiv;
 
@@ -9,22 +5,26 @@ window.onload = function () {
     // called when the page has been loaded
     video = document.querySelector("#myVideo");
     trackStatusesDiv = document.querySelector("#trackStatusesDiv");
+
     // Get the tracks as HTML elements
     htmlTracks = document.querySelectorAll("track");
-    // displays their statuses in a div under the video
-    displayTrackStatuses(htmlTracks);
+
+    // display their status in a div under the video
+    displayTrackStatuses();
 };
 
-function displayTrackStatuses(htmlTracks) {
-    // displays track info
+function displayTrackStatuses() {
+    trackStatusesDiv.innerHTML = "";
+
+    // display track info
     for (var i = 0; i < htmlTracks.length; i++) {
         var currentHtmlTrack = htmlTracks[i];
+
         var label = "<li>label = " + currentHtmlTrack.label + "</li>";
         var kind = "<li>kind = " + currentHtmlTrack.kind + "</li>";
         var lang = "<li>lang = " + currentHtmlTrack.srclang + "</li>";
-        var readyState = "<li>readyState = "
-            + currentHtmlTrack.readyState + "</li>"
-        trackStatusesDiv.innerHTML += "<li><b>Track:" + i + ":</b></li>"
-            + "<ul>" + label + kind + lang + readyState + "</ul>";
+        var readyState = "<li>readyState = " + currentHtmlTrack.readyState + "</li>";
+
+        trackStatusesDiv.innerHTML += "<li><b>Track:" + i + ":</b></li>" + "<ul>" + label + kind + lang + readyState + "</ul>";
     }
 }

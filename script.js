182c6cbf293fcd9e0295ac2427cb77dd0cc48e85
DoItReal <66225172+DoItReal@users.noreply.github.com>
2022-04-08 21:49:54 +0300

    analayzer v0.1

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 var video, htmlTracks, htmlTracks;
 var trackStatusesDiv, cuesDiv, cuesLiveDiv, spanTranscript, langButtonDiv, currentLangSpan;
 var filters = [];
-var canvasVisBefore, canvasVisBeforeContext, canvasVisAfter, canvasVisAfterContext;
+var canvasVisBefore, canvasVisBeforeContext, canvasVisBeforeType, canvasVisAfter, canvasVisAfterContext, canvasVisAfterType;
 var analyserBefer, dataArrayBefore, bufferLengthBefore, analyserAfter, dataArrayAfter, bufferLengthAfter;
-
+var widthBefore, heightBefore, widthAfter, heightAfter;
 //web audio API
 var gainExample, gainSlider, gainNode, balanceSlider, balanceNode, compressorButton, compressorNode, biquadfilterNode;
 var ctx = window.AudioContext || window.webkitAudioContext;
 var audioContext = new ctx();
 var compressorOn = false;
 
+canvasVisBeforeType = "wave";
+canvasVisAfterType = "wave";
 window.onload = function () {
     // called when the page has been loaded
     langButtonDiv = document.querySelector("#langButtonDiv");
@@ -33,7 +35,11 @@ window.onload = function () {
 
     canvasVisAfter = document.getElementById("canvasVisualizerAfter");
     canvasVisAfterContext = canvasVisAfter.getContext('2d');
-    canvasVisAfterContext.fillRect(100, 100, 200, 200);
+
+    widthBefore = canvasVisBefore.width;
+    heightBefore = canvasVisBefore.height;
+    widthAfter = canvasVisAfter.width;
+    heightAfter = canvasVisAfter.height;
 
     // display their status in a div under the video
     updateTrackStatuses();
@@ -95,6 +101,8 @@ function events() {
         }
         compressorOn = !compressorOn;
     };
+    // TO DO event change wave type on button click 
+ 
 
 
 }
@@ -130,9 +138,8 @@ function buildAudioGraph() {
     analyserBefore = audioContext.createAnalyser();
     // set visualizer options, for lower precision change 1024 to 512,
     // 256, 128, 64 etc. bufferLength will be equal to fftSize/2
-    analyserBefore.fftSize = 1024;
-    bufferLengthBefore = analyserBefore.frequencyBinCount;
-    dataArrayBefore = new Uint8Array(bufferLengthBefore);
+
+    analyserInit("before", 1024);
     source.connect(analyserBefore);
 
 
@@ -141,99 +148,212 @@ function buildAudioGraph() {
     filters[filters.length - 1].connect(analyserAfter);
     // set visualizer options, for lower precision change 1024 to 512,
     // 256, 128, 64 etc. bufferLength will be equal to fftSize/2
-    analyserAfter.fftSize = 1024;
-    bufferLengthAfter = analyserAfter.frequencyBinCount;
-    dataArrayAfter = new Uint8Array(bufferLengthAfter);
+    analyserInit("after", 1024);
     analyserAfter.connect(audioContext.destination);
 
 }
-function visualize() {
+function analyserInit(anal, fft) {
+    switch(anal){
+        case 'before':
+            analyserBefore.fftSize = fft;
+            bufferLengthBefore = analyserBefore.frequencyBinCount;
+            dataArrayBefore = new Uint8Array(bufferLengthBefore);
+            break;
+        case 'after':
+            analyserAfter.fftSize = fft;
+            bufferLengthAfter = analyserAfter.frequencyBinCount;
+            dataArrayAfter = new Uint8Array(bufferLengthAfter);
+            break;
+        default:
+            console.log("#analyserInit# unrecognized analyser: '" + anal + "'");
+    }
+
+}
+function changeVisualizerType(newType) {
+    cancelAnimationFrame(visBeforeAnimationId);
+
+    switch (newType) {
+        case 'wave':
+            if (canvasVisBeforeType == "wave" && canvasVisAfterType == "wave") break;
+           canvasVisBeforeType = "wave";
+            canvasVisAfterType = "wave";
+            analyserInit("before", 1024);
+            analyserInit("after", 1024);
+            break;
+        case 'bar':
+            if (canvasVisBeforeType == "bar" && canvasVisAfterType == "bar") break;
+           canvasVisBeforeType = "bar";
+            canvasVisAfterType = "bar";
+            analyserInit("before", 256);
+            analyserInit("after", 256);
+            break;
+        default:
+            console.log('Error #changeVisualizerType# unrecognized type: ' + newType);
+            break;
+    }
+    visBeforeAnimationId = requestAnimationFrame(visualize);
+}
+function visualizeClear() {
+    canvasVisBeforeContext.clearRect(0, 0, widthBefore, heightBefore);
+    canvasVisAfterContext.clearRect(0, 0, widthAfter, heightAfter);
+
 
     //blur 
     {
-        var widthBefore = canvasVisBefore.width;
-        var heightBefore = canvasVisBefore.height;
         canvasVisBeforeContext.fillStyle = 'rgba(0, 0, 0, 0.5)';
-        canvasVisBeforeContext.clearRect(0, 0, widthBefore, heightBefore);
         canvasVisBeforeContext.fillRect(0, 0, widthBefore, heightBefore);
 
-        var widthAfter = canvasVisAfter.width;
-        var heightAfter = canvasVisAfter.height;
         canvasVisAfterContext.fillStyle = 'rgba(0, 0, 0, 0.5)';
-        canvasVisAfterContext.clearRect(0, 0, widthAfter, heightAfter);
         canvasVisAfterContext.fillRect(0, 0, widthAfter, heightAfter);
     }
-    // 2 - Get the analyser data - for waveforms we need time domain data
-    analyserBefore.getByteTimeDomainData(dataArrayBefore);
+    canvasVisBeforeContext.font = '30px serif';
+    canvasVisAfterContext.font = '30px serif';
 
-    analyserAfter.getByteTimeDomainData(dataArrayAfter);
-    // 3 - draws the waveform
-    canvasVisBeforeContext.lineWidth = 2;
-    canvasVisBeforeContext.strokeStyle = 'lightBlue';
+    canvasVisBeforeContext.strokeText("Before Filters", 10, 25);
+    canvasVisAfterContext.strokeText("After Filters", 10, 25);
+}
+function visualizeGetAnalyserData() {
+    //left canvas
+    if (canvasVisBeforeType == "wave") {
+        analyserBefore.getByteTimeDomainData(dataArrayBefore);
+    } else if (canvasVisBeforeType == "bar") {
+        analyserBefore.getByteFrequencyData(dataArrayBefore);
+    } else console.log('Error unrecognized waveform: "' + canvasVisBeforeType + '"');
 
-    canvasVisAfterContext.lineWidth = 2;
-    canvasVisAfterContext.strokeStyle = 'lightBlue';
+    //right canvas
+    if (canvasVisAfterType == "wave") {
+        analyserAfter.getByteTimeDomainData(dataArrayAfter);
+    } else if (canvasVisAfterType == "bar") {
+        analyserAfter.getByteFrequencyData(dataArrayAfter);
+    } else console.log('Error unrecognized waveform: "' + canvasVisAfterType + '"');
+}
+function visDrawWaveForm(){
+        canvasVisBeforeContext.lineWidth = 2;
+        canvasVisBeforeContext.strokeStyle = 'lightBlue';
 
-    // the waveform is in one single path, first let's
-    // clear any previous path that could be in the buffer
-    canvasVisBeforeContext.beginPath();
-    var sliceWidth = widthBefore / bufferLengthBefore;
-    var x = 0;
+        canvasVisAfterContext.lineWidth = 2;
+        canvasVisAfterContext.strokeStyle = 'lightBlue';
 
-   
+        // the waveform is in one single path, first let's
+        // clear any previous path that could be in the buffer
+        canvasVisBeforeContext.beginPath();
+        var sliceWidth = widthBefore / bufferLengthBefore;
+        var x = 0;
 
-//before
-    {
-        for (let i = 0; i < bufferLengthBefore; i++) {
-            // dataArray values are between 0 and 255,
-            // normalize v, now between 0 and 1
-            let v = dataArrayBefore[i] / 255;
-            // y will be in [0, canvas height], in pixels
-            let y = v * heightBefore;
 
-            if (i === 0) {
-                canvasVisBeforeContext.moveTo(x, y);
-            } else {
-                canvasVisBeforeContext.lineTo(x, y);
+        //before
+        {
+            for (let i = 0; i < bufferLengthBefore; i++) {
+                // dataArray values are between 0 and 255,
+                // normalize v, now between 0 and 1
+                let v = dataArrayBefore[i] / 255;
+                // y will be in [0, canvas height], in pixels
+                let y = v * heightBefore;
+
+                if (i === 0) {
+                    canvasVisBeforeContext.moveTo(x, y);
+                } else {
+                    canvasVisBeforeContext.lineTo(x, y);
+                }
+
+                x += sliceWidth;
             }
 
-            x += sliceWidth;
-        }
+            canvasVisBeforeContext.lineTo(widthBefore, heightBefore / 2);
+            // draw the path at once
+            canvasVisBeforeContext.stroke();
 
-        canvasVisBeforeContext.lineTo(widthBefore, heightBefore / 2);
-        // draw the path at once
-        canvasVisBeforeContext.stroke();
-        canvasVisBeforeContext.font = '30px serif';
-        canvasVisBeforeContext.strokeText("Before Filters",10,25);
-    }
-    //after
-    canvasVisAfterContext.beginPath();
-    var sliceWidthAfter = widthAfter / bufferLengthAfter;
-    x = 0;
-    {
-        for (let i = 0; i < bufferLengthAfter; i++) {
-            // dataArray values are between 0 and 255,
-            // normalize v, now between 0 and 1
-            let v = dataArrayAfter[i] / 255;
-            // y will be in [0, canvas height], in pixels
-            let y = v * heightAfter;
-            if (i === 0) {
-                canvasVisAfterContext.moveTo(x, y);
-            } else {
-                canvasVisAfterContext.lineTo(x, y);
+        }
+        //after
+        canvasVisAfterContext.beginPath();
+        var sliceWidthAfter = widthAfter / bufferLengthAfter;
+        x = 0;
+        {
+            for (let i = 0; i < bufferLengthAfter; i++) {
+                // dataArray values are between 0 and 255,
+                // normalize v, now between 0 and 1
+                let v = dataArrayAfter[i] / 255;
+                // y will be in [0, canvas height], in pixels
+                let y = v * heightAfter;
+                if (i === 0) {
+                    canvasVisAfterContext.moveTo(x, y);
+                } else {
+                    canvasVisAfterContext.lineTo(x, y);
+                }
+
+                x += sliceWidthAfter;
             }
 
-            x += sliceWidthAfter;
+            canvasVisAfterContext.lineTo(widthAfter, heightAfter / 2);
         }
-
-        canvasVisAfterContext.lineTo(widthAfter, heightAfter / 2);
         // draw the path at once
         canvasVisAfterContext.stroke();
-        canvasVisAfterContext.font = '30px serif';
-        canvasVisAfterContext.strokeText("After Filters", 10, 25);
 }
+function visDrawBarForm() {
+    //   console.log(canvasVisBeforeType + " " + canvasVisAfterType);
+    //before
+    var barWidthBefore = canvasVisBefore.width / bufferLengthBefore;
+    var barHeightBefore;
+    var x = 0;
+    // values go from 0 to 255 and the canvas heigt is 100. Let's rescale
+    // before drawing. This is the scale factor
+    var heightScaleBefore = canvasVisBefore.height / 128;
+    for (var i = 0; i < bufferLengthBefore; i++) {
+        // between 0 and 255
+        barHeightBefore = dataArrayBefore[i];
+
+        // The color is red but lighter or darker depending on the value
+        canvasVisBeforeContext.fillStyle = 'rgb(' + (barHeightBefore + 100) + ',50,50)';
+        // scale from [0, 255] to the canvas height [0, height] pixels
+        barHeightBefore *= heightScaleBefore;
+        // draw the bar
+        canvasVisBeforeContext.fillRect(x, canvasVisBefore.height - barHeightBefore / 2, barWidthBefore, barHeightBefore / 2);
+
+        // 1 is the number of pixels between bars - you can change it
+        x += barWidthBefore + 1;
+    }
+
+    //after
+    var barWidthAfter = canvasVisAfter.width / bufferLengthAfter;
+    var barHeightAfter;
+    var x = 0;
+    // values go from 0 to 255 and the canvas heigt is 100. Let's rescale
+    // before drawing. This is the scale factor
+    var heightScaleAfter = canvasVisAfter.height / 128;
+    for (var i = 0; i < bufferLengthAfter; i++) {
+        // between 0 and 255
+        barHeightAfter = dataArrayAfter[i];
+
+        // The color is red but lighter or darker depending on the value
+        canvasVisAfterContext.fillStyle = 'rgb(' + (barHeightAfter + 100) + ',50,50)';
+        // scale from [0, 255] to the canvas height [0, height] pixels
+        barHeightAfter *= heightScaleAfter;
+        // draw the bar
+        canvasVisAfterContext.fillRect(x, canvasVisAfter.height - barHeightAfter / 2, barWidthAfter, barHeightAfter / 2);
+
+        // 1 is the number of pixels between bars - you can change it
+        x += barWidthAfter + 1;
+    }
+}
+function visualize() {
+
+    // 1 - Clear, blur the canvas and add Before, After text
+    visualizeClear();
+
+    // 2 - Get the analyser data - for waveforms we need time domain data
+    visualizeGetAnalyserData(); 
+
+    // 3 - draws the waveform
+    if (canvasVisBeforeType == "wave" && canvasVisAfterType == "wave") visDrawWaveForm();
+    else if (canvasVisBeforeType == "bar" && canvasVisAfterType == "bar") visDrawBarForm();
+    else console.log("Error #visualize# 3- drawing waveforms input canvasVisBeforeType: " + canvasVisBeforeType + " canvasVisAfterType: " + canvasVisAfterType);
+
+    // 4 - draw volume meter
+    drawVolumeMeter(canvasVisBefore, canvasVisBeforeContext, analyserBefore, dataArrayBefore);
+    drawVolumeMeter(canvasVisAfter, canvasVisAfterContext, analyserAfter, dataArrayAfter);
+
     // once again call the visualize function at 60 frames/s
-    requestAnimationFrame(visualize);
+    visBeforeAnimationId = requestAnimationFrame(visualize);
 
 }
 function BiquadFilter(val, hz) {
@@ -373,4 +493,37 @@ function createButton(track) {
     });
     b.appendChild(document.createTextNode(track.label));
     langButtonDiv.appendChild(b);
+}
+
+
+function drawVolumeMeter(canvas,canvasContext, analyser, dataArray) {
+
+    canvasContext.save();
+    analyser.getByteFrequencyData(dataArray);
+    var average = getAverageVolume(dataArray);
+    // set the fill style to a nice gradient
+    gradient = canvasContext.createLinearGradient(0, 0, 0, canvas.height);
+    gradient.addColorStop(1, '#000000');
+    gradient.addColorStop(0.75, '#ff0000');
+    gradient.addColorStop(0.25, '#ffff00');
+    gradient.addColorStop(0, '#ffffff');
+    canvasContext.fillStyle = gradient;
+    // draw the vertical meter
+    canvasContext.fillRect(0, canvas.height - average, 25, canvas.height);
+    canvasContext.strokeStyle = 'gray';
+    canvasContext.lineWidth = '1px';
+    canvasContext.strokeRect(0, canvas.height - average, 25, canvas.height);
+    canvasContext.restore();
+}
+
+function getAverageVolume(array) {
+    var values = 0;
+    var average;
+    var length = array.length;
+    // get all the frequency amplitudes
+    for (var i = 0; i < length; i++) {
+        values += array[i];
+    }
+    average = values / length;
+    return average;
 }
